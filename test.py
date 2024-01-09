866d8e8a58a46f96e89046c0b0e6537dc95edd04
LeaBFLL <144031817+LeaBFLL@users.noreply.github.com>
2024-01-09 16:41:09 +0100

    Update test.py
    plusieurs ?

--- test.py
+++ test.py
@@ -7,18 +7,19 @@ def main(page: ft.Page):
     txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
 
     items = [1, 2, 3, 4]
-    for i in range(len(items)):
-        page.add(
-    ft.Row(
-        [
-            ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-            txt_number,
-            ft.IconButton(ft.icons.ADD, on_click=plus_click),
-        ],
-        alignment=ft.MainAxisAlignment.CENTER,
-    )
-)
-        
+    compteur = []
+    for item in items:
+        compteur.append(
+            ft.Row(
+                [
+                    ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                    txt_number,
+                    ft.IconButton(ft.icons.ADD, on_click=plus_click),
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+            )
+        ))
+    page.add(compteurs)
     def minus_click(e):
         txt_number.value = str(int(txt_number.value) - 1)
         page.update()
