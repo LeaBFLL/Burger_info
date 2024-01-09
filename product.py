de47332d1a6fb975b11c89f9db36ffda56f31afa
LeaBFLL <144031817+LeaBFLL@users.noreply.github.com>
2024-01-09 16:57:32 +0100

    Update product.py

--- product.py
+++ product.py
@@ -1,2 +1,19 @@
 class Product:
-  
+  def __init__(self, icon, prize):
+    self.icon = icon
+    self.prize = prize
+    return Row(
+                    [
+                        Text(self.icon, size=50),
+                        Text(self.prize),
+                        Container(
+                            width=100,
+                            content=TextField(
+                                value="0", read_only=True
+                            ),
+                        ),
+                        IconButton(icon=icons.ADD),
+                        IconButton(icon=icons.REMOVE),
+                    ],
+                    alignment=MainAxisAlignment.CENTER,
+                )
